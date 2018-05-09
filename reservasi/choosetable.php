64790096018acf2b2aa53ae7ae0acef5a57f75aa
Siti Alifah <sitialifah303@gmail.com>
2018-05-09 21:02:54 +0700

    edit sedikit

--- reservasi/choosetable.php
+++ reservasi/choosetable.php
@@ -54,7 +54,7 @@
 			<div class="row">
 				<div class="col-lg-12 p-b-30">
 					<div class="t-center">
-						<span class=c"tit2 t-center">
+						<span class="tit2 t-center">
 							Reservation
 						</span>
 
