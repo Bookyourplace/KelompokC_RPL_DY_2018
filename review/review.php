b4129582fe548d7f325bfa0b6e6f9332f7fb394f
Siti Alifah <sitialifah303@gmail.com>
2018-05-15 17:08:15 +0700

    perubahan tampilan di review

--- review/review.php
+++ review/review.php
@@ -45,8 +45,59 @@ session_start();
 
 <body>
 
-<?php include '../includes/user/header.php'?>
-   
+<header>
+		<!-- Header desktop -->
+		<div class="wrap-menu-header gradient1 trans-0-4">
+			<div class="container h-full">
+				<div class="wrap_header trans-0-3">
+					<!-- Logo -->
+					<div class="logo">
+						<a href="<?php echo $config['site_url'] ?>" >
+							<img src="<?php echo $config['site_url']?>assets/users/images/icons/logo1.png" alt="IMG-LOGO" data-logofixed="<?php echo $config['site_url'] ?>assets/users/images/icons/logo2-1.png">
+						</a>
+					</div>
+
+					<!-- Menu -->
+					<div class="wrap_menu p-l-45 p-l-0-xl">
+						<nav class="menu">
+							<ul class="main_menu">
+							
+								<li>
+									<a href="<?php echo $config['site_url'] ?>index.php">Home</a>
+								</li>
+
+								<li>
+									<a href="<?php echo $config['site_url'] ?>reservasi/reservation.php">Reservation</a>
+								</li>
+
+								<li>
+									<a href="<?php echo $config['site_url'] ?>menu/menu.php">Menu</a>
+								</li>			
+
+								<li>
+									<a href="<?php echo $config['site_url'] ?>about/about.php">About</a>
+								</li>
+
+								<li>
+									<a href="<?php echo $config['site_url'] ?>review/review.php">Review</a>
+								</li>
+
+								<li>
+									<a href="<?php echo $config['site_url'] ?>auth/login.php">Login</a>
+								</li>
+								<li>
+									<a href="<?php echo $config['site_url'] ?>auth/logout.php">Log Out</a>
+									<!-- kalok bisa tambahin syarat kalok blm login, gada tombol logout -->
+								</li>
+							</ul>
+						</nav>
+					</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</header>
+
 
 <!-- Title Page -->
     <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15" style="background-image: url(<?php echo $config['site_url']?>assets/users/images/review.jpg);">
