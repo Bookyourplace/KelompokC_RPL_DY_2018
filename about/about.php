1f01d2c24841026bc7239420b706803a1795fe7d
Siti Alifah <sitialifah303@gmail.com>
2018-05-18 15:17:03 +0700

    tes

--- about/about.php
+++ about/about.php
@@ -28,7 +28,7 @@
 	</section>
 
 
-	<!-- Our Story Nich -->
+	<!-- Our Story-->
 	<section class="bg2-pattern p-t-116 p-b-110 t-center p-l-15 p-r-15">
 		<span class="tit2 t-center">
 			Book Your Place
