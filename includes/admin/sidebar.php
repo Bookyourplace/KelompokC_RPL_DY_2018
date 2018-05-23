4658f16454f3405313e752fbf79b26a47ae66148
Siti Alifah <sitialifah303@gmail.com>
2018-05-23 08:05:56 +0700

    commentnin sedikit

--- includes/admin/sidebar.php
+++ includes/admin/sidebar.php
@@ -1,11 +1,11 @@
 <nav class="side-navbar">
           <!-- Sidebar Header-->
           <div class="sidebar-header d-flex align-items-center">
-            <div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
+            <!--<div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid rounded-circle"></div>
             <div class="title">
               <h1 class="h4">Mark Stephen</h1>
               <p>Web Designer</p>
-            </div>
+            </div>-->
           </div>
           <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
           <ul class="list-unstyled">
