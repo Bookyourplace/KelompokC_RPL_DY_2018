b1712b9c37a0342f4d3cb21112cc59293e36c220
Unknown <monitasurya@gmail.com>
2018-05-28 00:12:11 +0700

    title

--- payment/record_payment.php
+++ payment/record_payment.php
@@ -25,3 +25,4 @@
 
 <!-- Title Page -->
 <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15" style="background-image: url(<?php echo $config['site_url']?>assets/users/images/payment.jpg);">
+    <h2 class="tit6 t-center">
