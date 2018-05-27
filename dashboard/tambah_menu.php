a179389eb3d83500a78eb968145b68ef6228a1f3
Siti Alifah <sitialifah303@gmail.com>
2018-05-27 08:52:04 +0700

    include css dan perbaharui head

--- dashboard/tambah_menu.php
+++ dashboard/tambah_menu.php
@@ -1,6 +1,15 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BookYourPlace</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="all,follow">
+
+    <!--css-->
+    <?php include '../includes/admin/css.php'; ?>
 </head>
 <body>
 </body>
