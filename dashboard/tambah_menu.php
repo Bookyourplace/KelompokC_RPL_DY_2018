1661a49c03e750a52223b02550993b79aaf7ff65
Siti Alifah <sitialifah303@gmail.com>
2018-05-27 09:01:41 +0700

    buat ada jarak dari header ke kontainer

--- dashboard/tambah_menu.php
+++ dashboard/tambah_menu.php
@@ -62,6 +62,11 @@
                     <li class="breadcrumb-item active">Tambah Menu            </li>
                     </ul>
             </div>
+                <!-- Container fluid  -->
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
         </div>
     </div>
 
