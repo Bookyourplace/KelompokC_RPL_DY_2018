cd6a512b6dcd0c7c1e5f369ff01d3763149ccf29
Siti Alifah <sitialifah303@gmail.com>
2018-05-27 08:15:41 +0700

    tambah class page

--- dashboard/list_menu.php
+++ dashboard/list_menu.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>BookYourPlace</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="robots" content="all,follow">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BookYourPlace</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="robots" content="all,follow">
 
-     <!--css-->
-     <?php include '../includes/admin/css.php'; ?>
-  <head>
-
-  </head>
-  <body>
-    <header>
-    
-    </header>   
-    
-  </body>
+        <!--css-->
+        <?php include '../includes/admin/css.php'; ?>
+    </head>
+    <body>
+    <div class="page">
+        <header>
+        
+        </header>   
+    </div>
+    </body>
  </html>
