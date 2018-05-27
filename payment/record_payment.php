9114db0358780faf3cce16ff3e6ed850de671395
Unknown <monitasurya@gmail.com>
2018-05-28 00:24:25 +0700

    html payment masih

--- payment/record_payment.php
+++ payment/record_payment.php
@@ -27,3 +27,4 @@
 <section class="bg-title-page flex-c-m p-t-160 p-b-80 p-l-15 p-r-15" style="background-image: url(<?php echo $config['site_url']?>assets/users/images/payment.jpg);">
     <h2 class="tit6 t-center">
     RECORD PAYMENT
+    </h2>
