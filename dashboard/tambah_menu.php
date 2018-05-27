e7fde4318ffdca23890a81a83fe34e0bc0d4d6ba
Siti Alifah <sitialifah303@gmail.com>
2018-05-27 08:54:24 +0700

    code konek ke db

--- dashboard/tambah_menu.php
+++ dashboard/tambah_menu.php
@@ -1,3 +1,29 @@
+<?php
+  session_start();
+  include '../konfig/config.php'; 
+  require '../konfig/koneksi.php';
+  //var_dump($_POST);
+
+  if (!empty($_POST)) {
+    $id_menu = htmlentities(strip_tags($_POST['id_menu']));
+    $nama_menu = htmlentities(strip_tags($_POST['nama_menu']));
+    $id_kategori = htmlentities(strip_tags($_POST['id_kategori']));
+    $harga = htmlentities(strip_tags($_POST['harga']));
+
+    $sql = "INSERT INTO menu(id_menu, nama_menu, id_kategori, harga) values('$id_menu', '$nama_menu', '$id_kategori', '$harga')";
+    $status = mysqli_query($link, $sql);
+
+
+    if ($status) {
+      $_SESSION['status_proses'] = "Menu Berhasil ditambahkan.";
+    }else{
+      $_SESSION['status_proses'] = "Menu Gagal ditambahkan: ".mysqli_error($link);
+    }
+
+    header('Location:list_menu.php');
+  }
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
