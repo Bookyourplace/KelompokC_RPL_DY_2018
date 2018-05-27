0db07c3c9c526798b399a30cd4d06d0359d6d7ab
Siti Alifah <sitialifah303@gmail.com>
2018-05-27 07:54:22 +0700

    include css nya

--- dashboard/list_menu.php
+++ dashboard/list_menu.php
@@ -1,5 +1,14 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BookYourPlace</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="all,follow">
+
+     <!--css-->
+     <?php include '../includes/admin/css.php'; ?>
   <head>
 
   </head>
