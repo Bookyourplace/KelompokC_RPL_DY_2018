842e1036c6caa78f48534b29febdae77b42e41a2
sitialifah <sitialifah303@gmail.com>
2018-05-28 21:10:09 +0700

    perbaharui syntax sql

--- dashboard/tambah_menu.php
+++ dashboard/tambah_menu.php
@@ -11,6 +11,8 @@
     $harga = htmlentities(strip_tags($_POST['harga']));
 
     //$sql = "INSERT INTO menu(id_menu, nama_menu, id_kategori, harga) values('$id_menu', '$nama_menu', '$id_kategori', '$harga')";
+    
+    $sql =sprintf("INSERT INTO menu(id_menu, nama_menu, id_kategori, harga) values('%s', '%s', '%s', '%s')",$id_menu, $nama_menu, $tes,$harga);
     $status = mysqli_query($link, $sql);
 
 
