2fe6dfffd7b278e757bbed354f982191b7e31580
Unknown <apryanto178@gmail.com>
2018-05-28 10:05:11 +0700

    code-3

--- auth/reset.php
+++ auth/reset.php
@@ -3,4 +3,7 @@
     include '../konfig/config.php';
     require_once '../konfig/koneksi.php';
 
-    $result = mysqli_query($link, "SELECT * FROM users");
+        $result = mysqli_query($link, "SELECT * FROM users");
+    
+         //query berikut akan jalan jika button Ubah Password diklik
+         
