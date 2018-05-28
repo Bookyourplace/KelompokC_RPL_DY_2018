555046cc5515acc1a32fb164bab115b487510589
Unknown <monitasurya@gmail.com>
2018-05-28 11:36:46 +0700

    html payment

--- payment/record_payment.php
+++ payment/record_payment.php
@@ -32,8 +32,11 @@
 
     <section class="section-reservation bg1-pattern p-t-100 p-b-113">
     <div class="">
-    <div class="row">
-    <div class="col-lg-12 p-b-30">
-    <div class="t-center">
-    <span class="tit2 t-center">
-    YOUR    
+     <div class="row">
+      <div class="col-lg-12 p-b-30">
+       <div class="t-center">
+         <span class="tit2 t-center">
+          YOUR    
+         </span>
+
+         
