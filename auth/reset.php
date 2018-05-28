87a2b34b6269eb8a53de93eda84d0baabc10a206
Unknown <apryanto178@gmail.com>
2018-05-28 10:21:02 +0700

    php reset

--- auth/reset.php
+++ auth/reset.php
@@ -48,3 +48,6 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="all,follow">
+
+    
+    <?php include '../includes/admin/css.php'?>
