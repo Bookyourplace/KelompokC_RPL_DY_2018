2b0c30c29033dee97079eb3bc4e6c016ca1f8033
Unknown <apryanto178@gmail.com>
2018-05-28 10:08:43 +0700

    code5

--- auth/reset.php
+++ auth/reset.php
@@ -8,3 +8,8 @@
          //query berikut akan jalan jika button Ubah Password diklik
          if(isset($_POST['reset'])){
             $errors = 0;
+             //memasukkan data yang diinput kedalam masing-masing variabel
+        $email= mysqli_real_escape_string($link,$_POST['email']);
+        $passwordlama = mysqli_real_escape_string($link,$_POST['passwordlama']);
+        $passwordbaru= mysqli_real_escape_string($link,$_POST['passwordbaru']);
+        $confirmpassword= mysqli_real_escape_string($link,$_POST['confirmpassword']);
