a21c50b100bef2c5dd824b3b8b72642a11cbe28d
Unknown <monitasurya@gmail.com>
2018-05-28 12:01:55 +0700

    syntax db salah

--- payment/record_payment.php
+++ payment/record_payment.php
@@ -56,4 +56,5 @@
 
            <?php
                 $q = "SELECT pesanan.id_pesanan, pesanan.date, pesanan.time, pesanan.people, book.id_meja FROM pesanan, book WHERE pesanan.id_pesanan=book.id_pesanan  AND pesanan.username='".$_SESSION['login_user']."'";
+                $query = mysqli_query($link, $q);
            ?>
