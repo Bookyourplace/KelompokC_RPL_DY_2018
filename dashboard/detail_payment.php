7b8cc3e8ce4cf1f738b991d514a8abf55598bf22
Unknown <monitasurya@gmail.com>
2018-06-01 00:35:47 +0700

    table back end 2

--- dashboard/detail_payment.php
+++ dashboard/detail_payment.php
@@ -86,3 +86,5 @@
                             $sql = "SELECT menu_pesanan.quant, menu_pesanan.harga_pesanan_persatuan, menu.nama_menu, (menu_pesanan.quant * menu_pesanan.harga_pesanan_persatuan) as amount FROM `menu_pesanan` join menu on menu.id_menu = menu_pesanan.id_menu where id_pesanan = $id_pesanan";
                             $data = mysqli_query($link, $sql);
                             $total = 0;
+
+                            for ($i=1; $row = mysqli_fetch_assoc($data); $i++) {
