9d30a946ebed56bf3c2fa1f717b361f4d185b658
Unknown <monitasurya@gmail.com>
2018-06-01 00:12:24 +0700

    cek login

--- dashboard/detail_payment.php
+++ dashboard/detail_payment.php
@@ -2,3 +2,6 @@
     session_start();
     include '../konfig/config.php'; 
     require '../konfig/koneksi.php';
+
+    if (!isset($_SESSION['login_user']))
+        header('Location:../login.php');
