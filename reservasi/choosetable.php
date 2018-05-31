31240179bcf26a796e245dca4c38096788547b03
sitialifah <sitialifah303@gmail.com>
2018-05-31 12:00:05 +0700

    tambah class

--- reservasi/choosetable.php
+++ reservasi/choosetable.php
@@ -85,10 +85,11 @@
 						<h3 class="tit3 t-center m-b-35 m-t-2">
 							Choose Table
 						</h3>
-						<div style="font-size: 16px">
+						<div class="tit2 t-center" style="font-size: 16px" >
 							<!-- <table> -->
 								<!-- <tr> -->
 									Date <?php echo $date ?>
+									
 									Time <?php echo $time ?>
 								<!-- </tr> -->
 							<!-- </table> -->
