614d7b78a35756c3d785da47b7bacedf3d2ff485
Unknown <monitasurya@gmail.com>
2018-05-31 14:10:05 +0700

    query ambil data

--- dashboard/confirm_payment.php
+++ dashboard/confirm_payment.php
@@ -8,3 +8,7 @@
 
         if(isset($_GET['notif']) && $_GET['notif'] == 1)
         echo "<script>alert('Pembayaran Berhasil!');</script>";
+
+        $sql = "SELECT pesanan.*, (select sum(menu_pesanan.harga_pesanan_persatuan * menu_pesanan.quant) from menu_pesanan where menu_pesanan.id_pesanan=pesanan.id_pesanan) total_harga FROM `pesanan`";
+        $data = mysqli_query($link, $sql);
+    ?>
