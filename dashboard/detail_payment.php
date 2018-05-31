535fe66c201602e7052e16548de00eabbfb24b79
Unknown <monitasurya@gmail.com>
2018-06-01 00:25:21 +0700

    title page

--- dashboard/detail_payment.php
+++ dashboard/detail_payment.php
@@ -21,3 +21,14 @@
         header('Location:' . $config['site_url'] . 'dashboard/confirm_payment.php');
 
         $id_pesanan = $_GET['id_pesanan'];
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>BookYourPlace</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="robots" content="all,follow">
