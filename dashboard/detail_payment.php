70942e2cf3842f17510da46e914d5e1268863506
Unknown <monitasurya@gmail.com>
2018-06-01 00:14:52 +0700

    query get submit

--- dashboard/detail_payment.php
+++ dashboard/detail_payment.php
@@ -5,3 +5,8 @@
 
     if (!isset($_SESSION['login_user']))
         header('Location:../login.php');
+
+        if(isset($_POST['submit'])){
+            $id_pesanan = $_GET['id_pesanan'];
+            $total = $_POST['total'];
+            //$yang_dibayar = $_POST['yang_dibayar'];
