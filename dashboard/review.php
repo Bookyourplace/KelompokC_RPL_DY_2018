c02600b770681c7ff44009514b01d775dda9ccd3
Raihana akmalia <raihanaakmalia18@gmail.com>
2018-05-31 14:31:38 +0700

    cssnya

--- dashboard/review.php
+++ dashboard/review.php
@@ -9,3 +9,17 @@
     $sql = "SELECT * FROM review";
     $data = mysqli_query($link,$sql);
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+    <meta charset="utf-8"
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BookYourPlace</title>
+    <meta name="description" content="">
+    <meta nama="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="all,follow">
+    
+    <?php include '../includes/admin/css.php'; ?>
+    </head>
+
