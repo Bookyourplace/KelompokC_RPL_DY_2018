81e818d3e601f36cd8ccca29db3ae201aedb5807
Raihana akmalia <raihanaakmalia18@gmail.com>
2018-05-31 15:03:11 +0700

    tampilan

--- dashboard/data_user.php
+++ dashboard/data_user.php
@@ -1,2 +1,10 @@
 <?php
     <!DOCTYPE html>
+    <html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BookYourPlace</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="robots" content="all,follow">
