c687bfc4e4ff34f85fe011b74fd32053c1164954
Raihana akmalia <raihanaakmalia18@gmail.com>
2018-05-31 14:48:32 +0700

    tampilin username

--- dashboard/review.php
+++ dashboard/review.php
@@ -73,5 +73,6 @@
 
         
         <tr style="background-color: #C3DBC5;">
-          <td col width="50" align="center"><?php echo $i; ?></td>     
+          <td col width="50" align="center"><?php echo $i; ?></td>
+          <td col width="180" align="left"><?php echo $row['username']; ?></td>     
         </tr>
