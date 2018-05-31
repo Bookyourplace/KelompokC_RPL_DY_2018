e63f419e805874ed57657d08934e58eb86343ac8
sitialifah <sitialifah303@gmail.com>
2018-05-31 13:58:35 +0700

    hapus side bar reservasi

--- includes/admin/sidebar.php
+++ includes/admin/sidebar.php
@@ -11,8 +11,7 @@
           <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
           <ul class="list-unstyled">
                     <li><a href="index.php"> <i class="icon-home"></i>Home </a></li>
-                    <li><a href="data_user.php"> <i class="icon-user"></i>Data User </a></li>
-                    <li><a href="reservasi.php"> <i class="icon-grid"></i>Reservasi </a></li>        
+                    <li><a href="data_user.php"> <i class="icon-user"></i>Data User </a></li>     
                     <li><a href="review.php"> <i class="icon-padnote"></i>Review </a></li>
                     <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse"> <i class="icon-interface-windows"></i>Menu </a>
                       <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
