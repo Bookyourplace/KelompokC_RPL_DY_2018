65e622f4287f682c5a9fbec2ec3bacdf5d7b9a26
Raihana akmalia <raihanaakmalia18@gmail.com>
2018-05-31 14:38:45 +0700

    page headernya

--- dashboard/review.php
+++ dashboard/review.php
@@ -33,3 +33,12 @@
 
     <!--sidebar-->
     <?php include '../includes/admin/sidebar.php'?>
+
+    <div class="content-inner">
+      <!-- page header-->
+      <header class="page-header">
+        <div class="container-fluid">
+          <h2 class="no-margin-bottom">Semua Review</h2>
+           
+        </div>
+      </header>
